63a25b988df940a7ccaa0f14db3b4c0291ab9b7e
chitradip97 <chitradip97@gmail.com>
2024-01-16 19:54:03 +0530

    done

--- bg_color_changer/src/App.js
+++ bg_color_changer/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setcolor] = useState("olive");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 px-2">
+        <button
+          onClick={() => {
+            setcolor("red");
+          }}
+          className="bg-white text-black px-4 rounded-full"
         >
-          Learn React
-        </a>
-      </header>
+          Red
+        </button>
+        <button
+          onClick={() => {
+            setcolor("blue");
+          }}
+          className="bg-white text-blue px-4 rounded-full"
+        >
+          blue
+        </button>
+        <button
+          onClick={() => {
+            setcolor("yellow");
+          }}
+          className="bg-white text-yellow px-4 rounded-full"
+        >
+          yellow
+        </button>
+        <button
+          onClick={() => {
+            setcolor("green");
+          }}
+          className="bg-white text-green px-4 rounded-full"
+        >
+          green
+        </button>
+        <button
+          onClick={() => {
+            setcolor("white");
+          }}
+          className="bg-white text-black px-4 rounded-full"
+        >
+          White
+        </button>
+      </div>
     </div>
   );
 }
